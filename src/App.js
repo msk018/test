12e0397bcf00ba69c437bd56e69a878903575cc1
meihua-li-0101 <hua860101@naver.com>
2021-08-26 11:16:38 +0900

    test page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 
 import './App.css';
-import { Route } from 'react-router';
+import { Route, Link } from 'react-router-dom';
 import Login from "./page/login"
 import Join from './page/join';
+import Home from './page/home';
 
 
 
 function App() {
+  
   return (
     <>
-    App
-    <Route path='/' exact component={Login} /> 
+    <div>
+    <Link to='/'>Home</Link>
+    </div>
+    
+    <div>
+    <Link to='/login'>Login</Link>
+    </div>
+    <div>
+    <Link to='/join'>Join</Link>
+    </div>
+    
+    <Route to='/' component={Home} />
+    <Route path='/login' exact component={Login} /> 
     <Route path='/join' exact component={Join} /> 
     </>
   );
---
+++ src/page/home.js
@@ -0,0 +1,13 @@
+import { directive } from "@babel/types"
+import React from "react"
+
+
+const Home =() => {
+    return(
+        <div>
+          Home
+        </div>
+    )
+}
+
+export default Home;
